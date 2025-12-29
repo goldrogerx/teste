ce7370b5e4518f1532daed0e7a660bf1d29578ea
Roger Curso Alura <goldrogerk@gmail.com>
2025-12-29 15:19:46 -0300

    adicionando exclamacoes

--- 3386-git-github-projeto_inicial/app.js
+++ 3386-git-github-projeto_inicial/app.js
@@ -10,8 +10,8 @@ function exibirTextoNaTela(tag, texto) {
 }
 
 function exibirMensagemInicial() {
-    exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
+    exibirTextoNaTela('h1', 'Jogo do número secreto!');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 100!');
 }
 
 exibirMensagemInicial();
